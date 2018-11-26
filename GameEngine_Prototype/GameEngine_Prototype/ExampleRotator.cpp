2f347a407f2ee7a36a412c03473f038b85a0d67a
Michael Wolf <michaelwolf95@gmail.com>
2018-11-26 00:34:42 -0800

    Commented ExampleRotator

--- GameEngine_Prototype/GameEngine_Prototype/ExampleRotator.cpp
+++ GameEngine_Prototype/GameEngine_Prototype/ExampleRotator.cpp
@@ -2,29 +2,31 @@
 #include "GameObject.h"
 #include "Time.h"
 #include "Input.h"
-#include "ApplicationManager.h"
-
 
 ExampleRotator::ExampleRotator() {}
 ExampleRotator::~ExampleRotator() {}
 
-void ExampleRotator::Start(){}
+void ExampleRotator::Start() {}
 
 void ExampleRotator::Update()
 {
-	float deltaY = rotationSpeed * Time::deltaTime;
-
+	// Get the "horizontal" input for the arrow keys. 
+	// Left is -1, Right is 1, and none is 0.
 	float horizontal = 0;
 	if (Input::GetKey(GLFW_KEY_LEFT))
 		horizontal += -1;
 	if (Input::GetKey(GLFW_KEY_RIGHT))
 		horizontal += 1;
 
-	deltaY *= horizontal;
+	// Get the change in rotation based on the input, speed, and change in time.
+	float deltaY = horizontal * rotationSpeed * Time::deltaTime;
 
+	// Get the current rotation of the object in degrees.
 	glm::vec3 rot = gameObject->transform->getLocalRotationEuler();
 
+	// Create a new rotation with y-rotation changed by deltaY.
 	glm::vec3 newRot = glm::vec3(rot.x, rot.y + deltaY, rot.z);
-	gameObject->transform->setLocalRotationEuler(newRot);
 
+	// Apply the new rotation to the transform.
+	gameObject->transform->setLocalRotationEuler(newRot);
 }
