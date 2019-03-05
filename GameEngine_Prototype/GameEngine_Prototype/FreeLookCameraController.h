d5da1bd50bc94d95ddc5d3b0b59ae3191b69af44
tamqtran <tamtran1997@gmail.com>
2019-03-05 14:44:25 -0800

    Revert "Fixed FreeLookCamera to match Editor Camera"
    This reverts commit 26146819d221654027107da385352d38bf82bff0.

--- GameEngine_Prototype/GameEngine_Prototype/FreeLookCameraController.h
+++ GameEngine_Prototype/GameEngine_Prototype/FreeLookCameraController.h
@@ -1,22 +1,14 @@
 #pragma once
 #include "Component.h"
-#include <glm/glm.hpp>
 class FreeLookCameraController : public Component
 {
 public:
-	float zoomSpeed = 15.0f;
-	float xRotSpeed = 0.05f;
-	float yRotSpeed = 0.05f;
-	float panSpeed = 0.1f;
+	float moveSpeed = 2.5;
+	float lookSensitivity = 0.1f;
 	//float fov = 45.0f;
 	FreeLookCameraController();
 	~FreeLookCameraController();
 	void Start() override;
 	void Update() override;
-private:
-	unsigned int camMode;
-	glm::vec2 clickPos;
-	glm::vec2 lastDragPos;
-	
 };
 
