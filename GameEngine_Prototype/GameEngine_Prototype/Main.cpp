253f264adbe52725c1094ebb9ff428bb763bf1ee
Michaelwolf95 <michaelwolf95@gmail.com>
2019-03-06 14:45:34 -0800

    imGUI test implementation (unfinished)

--- GameEngine_Prototype/GameEngine_Prototype/Main.cpp
+++ GameEngine_Prototype/GameEngine_Prototype/Main.cpp
@@ -1,7 +1,12 @@
 #define GLM_FORCE_RADIANS
 #include <iostream>
-#include <glad/glad.h>
-#include <GLFW/glfw3.h>
+
+#include <imgui/imgui.h>
+#include <imgui/imgui_impl_glfw.h>
+#include <imgui/imgui_impl_opengl3.h>
+
+//#include <glad/glad.h>
+//#include <GLFW/glfw3.h>
 #define STB_DEFINE  
 #define STB_IMAGE_IMPLEMENTATION
 #include <stb/stb_image.h>
@@ -22,10 +27,62 @@
 #include "TestScenes.h"
 #endif
 
+
+static void glfw_error_callback(int error, const char* description)
+{
+	fprintf(stderr, "Glfw Error %d: %s\n", error, description);
+}
+
 // ENTRY POINT
 int main()
 {
 	std::cout << "===== LAUNCHING X-ENGINE =====" << std::endl;
+	
+	// Setup window
+	glfwSetErrorCallback(glfw_error_callback);
+	if (!glfwInit())
+		return 1;
+
+	// Decide GL+GLSL versions
+#if __APPLE__
+	// GL 3.2 + GLSL 150
+	const char* glsl_version = "#version 150";
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 2);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);  // 3.2+ only
+	glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);            // Required on Mac
+#else
+	// GL 3.0 + GLSL 130
+	const char* glsl_version = "#version 130";
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 0);
+	//glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);  // 3.2+ only
+	//glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);            // 3.0+ only
+#endif
+
+	 // Create window with graphics context
+	GLFWwindow* window = glfwCreateWindow(1280, 720, "Dear ImGui GLFW+OpenGL3 example", NULL, NULL);
+	if (window == NULL)
+		return 1;
+	glfwMakeContextCurrent(window);
+	glfwSwapInterval(1); // Enable vsync
+
+
+	// Setup Dear ImGui context
+	IMGUI_CHECKVERSION();
+	ImGui::CreateContext();
+	ImGuiIO& io = ImGui::GetIO(); (void)io;
+	//io.ConfigFlags |= ImGuiConfigFlags_NavEnableKeyboard;  // Enable Keyboard Controls
+	//io.ConfigFlags |= ImGuiConfigFlags_NavEnableGamepad;   // Enable Gamepad Controls
+
+	// Setup Dear ImGui style
+	ImGui::StyleColorsDark();
+	//ImGui::StyleColorsClassic();
+
+	// Setup Platform/Renderer bindings
+	ImGui_ImplGlfw_InitForOpenGL(window, true);
+	ImGui_ImplOpenGL3_Init(glsl_version);
+	
 	// Init Managers
 	ApplicationManager::CreateManager();
 	Time::CreateManager();
@@ -44,6 +101,16 @@ int main()
 #else
 	RunTestScene();
 #endif
+	//float f = 2.0f;
+	//static char buf[32];
+	//ImGui::Text("Hello, world %d", 123);
+	//if (ImGui::Button("Save"))
+	//{
+	//	// do stuff
+	//}
+	//ImGui::InputText("string", buf, IM_ARRAYSIZE(buf));
+	//ImGui::SliderFloat("float", &f, 0.0f, 1.0f);
+
 	
 	// FRAME LOOP
 	while (!ApplicationManager::getInstance().CheckIfAppShouldClose())
---
+++ GameEngine_Prototype/GameEngine_Prototype/imgui_def.cpp
@@ -0,0 +1,6 @@
+#include <imgui/imgui.cpp>
+#include <imgui/imgui_impl_glfw.cpp>
+#include <imgui/imgui_impl_opengl3.cpp>
+#include <imgui/imgui_draw.cpp>
+#include <imgui/imgui_widgets.cpp>
+#include <imgui/imgui_demo.cpp>
