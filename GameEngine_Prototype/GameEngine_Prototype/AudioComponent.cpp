21a6e81a1ada525bf7d7f9cf3c99d27b55f1eace
james-pepper <drsand40@gmail.com>
2019-04-08 14:04:59 -0700

    3D audio working

--- GameEngine_Prototype/GameEngine_Prototype/AudioComponent.cpp
+++ GameEngine_Prototype/GameEngine_Prototype/AudioComponent.cpp
@@ -9,9 +9,7 @@
 #include "imgui_inspector_extensions.h"
 #include "imgui_stdlib.h"
 #include "Input.h"
-//CAudioEngine test;
-//soundTest->Init();
-//
+
 ////LoadSound(const std::string& strSoundName, bool b3d, bool bLooping, bool bStream)
 ////parameters about streaming, looping, and whether or not it's a 3D sound
 //soundTest->LoadSound(soundPath1, false, true, true);
@@ -21,10 +19,11 @@ REGISTER_COMPONENT(AudioComponent, "AudioComponent")
 
 void AudioComponent::Start() {
 
-	//string soundPath1 = "../Assets/sounds/foghorn.wav";
-	//soundList.push_back(soundPath1);
+	//string soundPath1 = "../Assets/sounds/foghorn.wav"; 
+	//../Assets/sounds/inception_sound.mp3
 	if (soundPath.size() > 0)
 	{
+		//add the looping in this just nest it for each option
 		if (is3D)
 		{
 			Load3D(soundPath, true, true, true);
@@ -35,11 +34,6 @@ void AudioComponent::Start() {
 		}
 	}
 	//gameObject->transform->getPosition();
-
-	glm::mat4 camView = RenderManager::getInstance().getCurrentCamera()->getView();
-	glm::vec3 camLocation(camView[3].x, camView[3].y, camView[3].z);
-	AudioManager::getInstance().sound.Set3dListenerAndOrientation(camLocation, camLocation, camLocation);
-	
 }
 
 void AudioComponent::Update()
@@ -70,21 +64,16 @@ AudioComponent::~AudioComponent()
 
 void AudioComponent::Load2D(string path, bool loop, bool stream)
 {
-	//CAudioEngine sound = AudioManager::getInstance();
-	//AudioManager::AudioEngine;
 	AudioManager::getInstance().sound.LoadSound(path, false, loop, stream);
-	
-	//AudioManager::getInstance().LoadSound(path, 3d, loop, stream);
 }
 
-void AudioComponent::Load3D(string path, bool location, bool loop, bool stream) {
-
-	if (location == true) {
-		AudioManager::getInstance().sound.LoadSound(path, location, loop, stream);
-	}
+void AudioComponent::Load3D(string path, bool location, bool loop, bool stream)
+{
+	AudioManager::getInstance().sound.LoadSound(path, location, loop, stream);
 }
 
-void AudioComponent::Play(string path, glm::vec3 location, float fVolumedB) {
+void AudioComponent::Play(string path, glm::vec3 location, float fVolumedB)
+{
 	//mChannel = AudioManager::getInstance().sound.PlaySounds(path, location, fVolumedB);
 	AudioManager::getInstance().sound.PlaySounds(path, location, fVolumedB);
 	//AudioManager::getInstance().sound.SetChannelVolume(mChannel, .1);
@@ -96,12 +85,12 @@ void AudioComponent::Play()
 	Play(soundPath, objectLocation, 1);
 }
 
-glm::vec3 AudioComponent::getListener()
-{
-	glm::mat4 camView = RenderManager::getInstance().getCurrentCamera()->getView();
-	glm::vec3 camLocation(camView[3].x, camView[3].y, camView[3].z);
-	return camLocation;
-}
+//glm::vec3 AudioComponent::getListener()
+//{
+//	glm::mat4 camView = RenderManager::getInstance().getCurrentCamera()->getView();
+//	glm::vec3 camLocation(camView[3].x, camView[3].y, camView[3].z);
+//	return camLocation;
+//}
 
 void AudioComponent::DrawInspector()
 {
@@ -114,10 +103,5 @@ void AudioComponent::DrawInspector()
 	}
 	// Flags
 	ImGui::Checkbox("Is 3D", &is3D);
+	//add loop
 }
-
-
-//void AudioComponent::Play3DSound(string path, ) {
-//	//void PlaySound(const string& strSoundName, const glm::vec3& vPos = glm::vec3{ 0, 0, 0 }, float fVolumedB = 0.0f);
-//	//PlaySound
-//}
