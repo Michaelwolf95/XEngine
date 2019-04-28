81b7666f2e46520aedb36a11aa5517c43ee95e65
Michael Wolf <michaelwolf95@gmail.com>
2019-04-27 19:14:43 -0700

    Added new boost libraries for dynamic linking.

---
+++ GameEngine_Prototype/GameEngine_Prototype/imgui_inspector_extensions.cpp
@@ -0,0 +1,73 @@
+#include "imgui.h"
+#include <iostream>
+#include <string>
+#include "XEngine.h"
+#include "Component.h"
+#include "GameObject.h"
+namespace ImGui
+{
+	//IMGUI_API void InputTextField(std::string& str, char* label = "##edit");
+	IMGUI_IMPL_API void InputTextField(std::string& str, const char* label)
+	{
+		//ImGui::Text("Edit name:");
+		char buf[32];
+		strcpy_s(buf, &str[0]);
+		ImGui::InputText(label, buf, 32);
+		if (buf != str)
+		{
+			//std::cout << buf << std::endl;
+			str.clear();
+			str = std::string(buf);
+		}
+		//ImGui::InputText("##edit", &selectedGameObject->name[0], 32, 0, 
+		//ImGui::InputText("##edit", inName, 32, 0,
+		//	([](ImGuiInputTextCallbackData* data)->int{
+		//	if (data->EventFlag == ImGuiInputTextFlags_CallbackCompletion)
+		//	{
+		//		std::string bufStr(data->Buf);
+		//		//if(bufStr != selectedGameObject->name)
+		//		//data->b
+		//	}
+		//	return 0;
+		//}));
+	}
+	// Helper to display a little (?) mark which shows a tooltip when hovered.
+	IMGUI_IMPL_API void HelpMarker(const char* desc)
+	{
+		ImGui::TextDisabled("(?)");
+		if (ImGui::IsItemHovered())
+		{
+			ImGui::BeginTooltip();
+			ImGui::PushTextWrapPos(ImGui::GetFontSize() * 35.0f);
+			ImGui::TextUnformatted(desc);
+			ImGui::PopTextWrapPos();
+			ImGui::EndTooltip();
+		}
+	}
+
+	IMGUI_IMPL_API void GameObjectReference(GameObject*& go, std::string label)
+	{
+		ImGui::Text(label.c_str());
+		ImGui::SameLine();
+		if (go != nullptr)
+		{
+			ImGui::Button(go->name.c_str());
+		}
+		else
+		{
+			ImGui::Button("(None)");
+		}
+		if (ImGui::BeginDragDropTarget())
+		{
+			if (const ImGuiPayload* payload = ImGui::AcceptDragDropPayload("GAMEOBJECT_DRAG"))
+			{
+				IM_ASSERT(payload->DataSize == sizeof(GameObject_ptr));
+				GameObject_ptr payload_n = *(const GameObject_ptr*)payload->Data;
+
+				go = payload_n.get();
+			}
+			ImGui::EndDragDropTarget();
+		}
+	}
+
+}
