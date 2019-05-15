c9d1401b19e0a672d1429462bf6a88b7faf8e0cc
tamqtran <tamtran1997@gmail.com>
2019-05-14 23:16:26 -0700

    finished 5/7 of the required integration tests, Render Management System

---
+++ GameEngine_Prototype/GameEngine_UnitTest/Integration_5_Render_Management_System.cpp
@@ -0,0 +1,255 @@
+#include "stdafx.h"
+#include "CppUnitTest.h"
+//
+#include "XEngine.h"
+#include "Transform.h"
+#include "MeshRenderer.h"
+#include "SphereCollider.h"
+#include "BoxCollider.h"
+#include "Rigidbody.h"
+#include "MeshRenderer.h"
+
+using namespace XEngine;
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace XEngine_UnitTest
+{
+	TEST_CLASS(Render_Management_System)
+	{
+	public:
+		TEST_METHOD(Test_GetView)
+		{
+			// Arrange
+			ENGINE_INITIALIZE();
+			Scene_ptr scene = SceneManager::getInstance().CreateNewScene("Test_SceneManagement_System");
+
+			glm::mat4 viewExpected= RenderManager::getInstance().getView();
+			glm::mat4 viewResult;
+
+			SceneManager::getInstance().SetActiveScene(scene);
+			SceneManager::getInstance().SaveActiveScene();
+
+			while (!ApplicationManager::getInstance().CheckIfAppShouldClose())
+			{
+				ApplicationManager::getInstance().ApplicationStartUpdate();
+				GameTime::getInstance().UpdateTime();
+				Input::getInstance().UpdateInput();
+
+				if (OnEngineUpdate != nullptr) OnEngineUpdate();
+				SceneManager::getInstance().UpdateActiveScene();
+				AudioManager::getInstance().UpdateAudio();
+
+				PhysicsManager::getInstance().PhysicsUpdate();
+
+				if (OnEnginePreRender != nullptr) OnEnginePreRender();
+
+				RenderManager::getInstance().Render();
+
+				// Act 
+				viewResult = RenderManager::getInstance().getView();
+
+				if (OnEnginePostRender != nullptr) OnEnginePostRender();
+
+				Input::getInstance().EndUpdateFrame();
+				ApplicationManager::getInstance().ApplicationEndUpdate();
+			}
+
+			if (OnApplicationClose != nullptr) OnApplicationClose();
+
+			ApplicationManager::getInstance().CloseApplication();
+
+			// Assert: if values are the same
+			Assert::IsTrue(viewExpected == viewResult);
+		}
+
+		TEST_METHOD(Test_GetProjection)
+		{
+			// Arrange
+			ENGINE_INITIALIZE();
+			Scene_ptr scene = SceneManager::getInstance().CreateNewScene("Test_SceneManagement_System");
+
+			glm::mat4 projExpected = RenderManager::getInstance().getProjection();
+			glm::mat4 projResult;
+
+			SceneManager::getInstance().SetActiveScene(scene);
+			SceneManager::getInstance().SaveActiveScene();
+
+			while (!ApplicationManager::getInstance().CheckIfAppShouldClose())
+			{
+				ApplicationManager::getInstance().ApplicationStartUpdate();
+				GameTime::getInstance().UpdateTime();
+				Input::getInstance().UpdateInput();
+
+				if (OnEngineUpdate != nullptr) OnEngineUpdate();
+				SceneManager::getInstance().UpdateActiveScene();
+				AudioManager::getInstance().UpdateAudio();
+
+				PhysicsManager::getInstance().PhysicsUpdate();
+
+				if (OnEnginePreRender != nullptr) OnEnginePreRender();
+
+				RenderManager::getInstance().Render();
+
+				// Act 
+				projResult = RenderManager::getInstance().getProjection();
+
+				if (OnEnginePostRender != nullptr) OnEnginePostRender();
+
+				Input::getInstance().EndUpdateFrame();
+				ApplicationManager::getInstance().ApplicationEndUpdate();
+			}
+
+			if (OnApplicationClose != nullptr) OnApplicationClose();
+
+			ApplicationManager::getInstance().CloseApplication();
+
+			// Assert: if values are the same
+			Assert::IsTrue(projExpected == projResult);
+		}
+
+		TEST_METHOD(Test_GetCurrentCamera)
+		{
+			// Arrange
+			ENGINE_INITIALIZE();
+			Scene_ptr scene = SceneManager::getInstance().CreateNewScene("Test_SceneManagement_System");
+
+			GameObject_ptr cameraExpected = scene->CreateGameObject("Camera");
+			std::shared_ptr<CameraComponent> cam(new CameraComponent());
+			cameraExpected->AddComponent(cam);
+			std::shared_ptr<AudioListener> listener(new AudioListener());
+			cameraExpected->AddComponent(listener);
+			Camera* cameraResult;
+
+			SceneManager::getInstance().SetActiveScene(scene);
+			SceneManager::getInstance().SaveActiveScene();
+
+			while (!ApplicationManager::getInstance().CheckIfAppShouldClose())
+			{
+				ApplicationManager::getInstance().ApplicationStartUpdate();
+				GameTime::getInstance().UpdateTime();
+				Input::getInstance().UpdateInput();
+
+				if (OnEngineUpdate != nullptr) OnEngineUpdate();
+				SceneManager::getInstance().UpdateActiveScene();
+				AudioManager::getInstance().UpdateAudio();
+
+				PhysicsManager::getInstance().PhysicsUpdate();
+
+				if (OnEnginePreRender != nullptr) OnEnginePreRender();
+
+				RenderManager::getInstance().Render();
+
+				// Act 
+				cameraResult = RenderManager::getInstance().getCurrentCamera();
+
+				if (OnEnginePostRender != nullptr) OnEnginePostRender();
+
+				Input::getInstance().EndUpdateFrame();
+				ApplicationManager::getInstance().ApplicationEndUpdate();
+			}
+
+			if (OnApplicationClose != nullptr) OnApplicationClose();
+
+			ApplicationManager::getInstance().CloseApplication();
+
+			// Assert: if values are the same
+			Assert::IsTrue(cam->getProjection() == cameraResult->getProjection());
+		}
+
+		TEST_METHOD(Test_AddRenderable)
+		{
+			// Arrange
+			ENGINE_INITIALIZE();
+			Scene_ptr scene = SceneManager::getInstance().CreateNewScene("Test_SceneManagement_System");
+
+			// Act Part 1: (this line calls RenderableObject) 
+			std::shared_ptr<XEngine::MeshRenderer> mesh(new XEngine::MeshRenderer());
+			mesh->OnEnable();
+			int count = 0;
+
+			SceneManager::getInstance().SetActiveScene(scene);
+			SceneManager::getInstance().SaveActiveScene();
+
+			while (!ApplicationManager::getInstance().CheckIfAppShouldClose())
+			{
+				ApplicationManager::getInstance().ApplicationStartUpdate();
+				GameTime::getInstance().UpdateTime();
+				Input::getInstance().UpdateInput();
+
+				if (OnEngineUpdate != nullptr) OnEngineUpdate();
+				SceneManager::getInstance().UpdateActiveScene();
+				AudioManager::getInstance().UpdateAudio();
+
+				PhysicsManager::getInstance().PhysicsUpdate();
+
+				if (OnEnginePreRender != nullptr) OnEnginePreRender();
+
+				RenderManager::getInstance().Render();
+
+				// Act Part 2: get count of renderable objects
+				count = RenderManager::getInstance().currentRenderables.size();
+
+				if (OnEnginePostRender != nullptr) OnEnginePostRender();
+
+				Input::getInstance().EndUpdateFrame();
+				ApplicationManager::getInstance().ApplicationEndUpdate();
+			}
+
+			if (OnApplicationClose != nullptr) OnApplicationClose();
+
+			ApplicationManager::getInstance().CloseApplication();
+
+			// Assert: if values are the same
+			Assert::IsTrue(count == 1);
+		}
+
+		TEST_METHOD(Test_RemoveRenderable)
+		{
+			// Arrange
+			ENGINE_INITIALIZE();
+			Scene_ptr scene = SceneManager::getInstance().CreateNewScene("Test_SceneManagement_System");
+
+			// Act Part 1: (this line calls RenderableObject) 
+			std::shared_ptr<XEngine::MeshRenderer> mesh(new XEngine::MeshRenderer());
+			mesh->OnEnable();
+			int count = 0;
+
+			SceneManager::getInstance().SetActiveScene(scene);
+			SceneManager::getInstance().SaveActiveScene();
+
+			while (!ApplicationManager::getInstance().CheckIfAppShouldClose())
+			{
+				ApplicationManager::getInstance().ApplicationStartUpdate();
+				GameTime::getInstance().UpdateTime();
+				Input::getInstance().UpdateInput();
+
+				if (OnEngineUpdate != nullptr) OnEngineUpdate();
+				SceneManager::getInstance().UpdateActiveScene();
+				AudioManager::getInstance().UpdateAudio();
+
+				PhysicsManager::getInstance().PhysicsUpdate();
+
+				if (OnEnginePreRender != nullptr) OnEnginePreRender();
+
+				RenderManager::getInstance().Render();
+
+				// Act Part 2: get count of renderable objects
+				mesh->OnDisable();
+				count = RenderManager::getInstance().currentRenderables.size();
+
+				if (OnEnginePostRender != nullptr) OnEnginePostRender();
+
+				Input::getInstance().EndUpdateFrame();
+				ApplicationManager::getInstance().ApplicationEndUpdate();
+			}
+
+			if (OnApplicationClose != nullptr) OnApplicationClose();
+
+			ApplicationManager::getInstance().CloseApplication();
+
+			// Assert: if values are the same
+			Assert::IsTrue(count == 0);
+		}
+	};
+}
